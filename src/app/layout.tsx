12c897afd7122d5aff84982e85d1e9a9eee35d5e
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-05-17 12:51:55 +0700

    feat(navbar layout): add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/molecules/navbar";
 
 const poppins = Poppins({
   weight: ["300", "400", "600", "700", "800", "900"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        <div className="mt-[68px]">{children}</div>
+        </body>
     </html>
   );
 }
---
+++ src/components/molecules/navbar.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+export const Navbar = () => {
+    return (
+        <nav className="fixed left-0 top-0 z-50 w-full h-16 flex justify-center items-center bg-white/90 px-2 py-0.5 backdrop-blur-2xl md:px-0">
+            <div className="container flex flex-col items-center md:flex-row">
+                <div className="flex w-full items-center justify-between gap-2">
+                    <div className="flex items-center gap-x-2">
+                        <h5 className="font-bold md:text-lg lg:text-2xl">
+                            Photo<span className="text-orangeprimary">code</span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
