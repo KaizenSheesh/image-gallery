866ec08376fef8fc57ec7687954f4db65c74fda2
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-05-17 12:54:09 +0700

    chore(tailwind.config): menambahkan class custom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      container: {
+        padding: {
+          DEFAULT: "0.2rem",
+          sm: "1rem",
+          md: "1rem",
+          lg: "0rem",
+        },
+        center: true,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        orangeprimary: "#FF6D00",
+      }
     },
   },
   plugins: [],
