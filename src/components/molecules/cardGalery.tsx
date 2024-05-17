553e8f34ae775e6ed131b5aa5527a11eee1b21b4
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-05-17 13:51:30 +0700

    update(card gallery): update card animasi saat dihover

--- src/components/molecules/cardGalery.tsx
+++ src/components/molecules/cardGalery.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Photo } from 'pexels';
+import Image from 'next/image';
 
 interface CardGaleryProps {
   image: Photo;
@@ -7,13 +8,15 @@ interface CardGaleryProps {
 
 export const CardGalery: React.FC<CardGaleryProps> = ({ image }) => {
   return (
-    <div className="relative overflow-hidden rounded shadow-lg">
-      <img
+    <div className="relative overflow-hidden rounded shadow-lg h-80 group">
+      <Image
+        layout="fill"
+        objectFit="cover"
+        src={image.src.medium}
+        alt={image.photographer}
         className="object-cover w-full h-full"
-        src={image.src.medium} 
-        alt={image.photographer} 
       />
-      <p className="absolute bottom-0 left-0 w-full p-2 bg-black bg-opacity-50 text-white text-sm truncate">
+      <p className="absolute bottom-[-50px] left-0 w-full p-2 bg-black bg-opacity-50 text-white text-sm truncate transition-all duration-300 group-hover:bottom-0">
         {image.photographer}
       </p>
     </div>
